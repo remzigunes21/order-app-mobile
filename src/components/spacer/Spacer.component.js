0a3cb56d243892257a562989cf23d62df61ecb91
remzigunes21 <remzi.gunescmp@gmail.com>
2022-11-24 22:09:12 +0300

    clg

--- src/components/spacer/Spacer.component.js
+++ src/components/spacer/Spacer.component.js
@@ -16,7 +16,7 @@ const getVariant = (position, size, theme) => {
   const sizeIndex = sizeVariant[size];
   const property = positionVariant[position];
   const value = theme.space[sizeIndex];
-
+  console.log("value");
   return `${property}:${value}`;
 };
 
