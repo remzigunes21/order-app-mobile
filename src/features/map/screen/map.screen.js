819bee08d65a6cdbc9f373616ee8a060ca2f9dcc
remzigunes21 <remzi.gunescmp@gmail.com>
2022-11-28 10:55:27 +0300

    add map callout and mapview

--- src/features/map/screen/map.screen.js
+++ src/features/map/screen/map.screen.js
@@ -1,18 +1,66 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, { useContext, useEffect, useState } from "react";
+import MapView, { Marker, Callout } from "react-native-maps";
 import styled from "styled-components/native";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/mock/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
 import { Search } from "../components/search.component";
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
+  align-self: stretch;
 `;
 
-export default function MapScreen() {
+const MapScreen = ({ navigation }) => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
     <>
       <Search />
-      <Map />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            >
+              <Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", { restaurant })
+                }
+              >
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
+          );
+        })}
+      </Map>
     </>
   );
-}
+};
+
+export default MapScreen;
