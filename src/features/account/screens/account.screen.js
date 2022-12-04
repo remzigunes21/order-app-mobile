89deef0774fac94280163277008a5518ce9ead0b
remzigunes21 <remzi.gunescmp@gmail.com>
2022-12-04 19:34:23 +0300

    add register scrreen

---
+++ src/features/account/screens/account.screen.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+  Title,
+} from "../components/account.styles";
+import Spacer from "../../../components/spacer/Spacer.component";
+export const AccountScreen = ({ navigation }) => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title>Food to Order</Title>
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="email"
+            mode="contained"
+            onPress={() => navigation.navigate("Register")}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
+};
--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -4,6 +4,9 @@ import {
   TransitionPresets,
 } from "@react-navigation/stack";
 import { Text, View } from "react-native";
+import { AccountScreen } from "../../features/account/screens/account.screen";
+import { LoginScreen } from "../../features/account/screens/login.screen";
+import { RegisterScreen } from "../../features/account/screens/register.screen";
 
 const Stack = createStackNavigator();
 
@@ -14,14 +17,9 @@ export const AccountsNavigator = () => {
         headerShown: false,
       }}
     >
-      <Stack.Screen
-        name="Main"
-        component={() => (
-          <View>
-            <Text>Login</Text>
-          </View>
-        )}
-      />
+      <Stack.Screen name="Main" component={AccountScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
